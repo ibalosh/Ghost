190697f70fb3f61fab44316e48f645358bd6293b
Igor Balos <ibalosh@gmail.com>
2025-10-10 16:01:34 +0200

    aded backfil migration

---
+++ ghost/core/core/server/data/migrations/versions/6.4/2025-10-10-13-23-52-backfill-reading-times.js
@@ -0,0 +1,18 @@
+const logging = require('@tryghost/logging');
+const {createTransactionalMigration} = require('../../utils');
+const {calculatePostReadingTime} = require('../../../../../shared/post-reading-times');
+
+module.exports = createTransactionalMigration(
+    async function up(knex) {
+        const posts = await knex.select('id', 'html', 'feature_image').from('posts').whereNotNull('html');
+
+        logging.info(`Adding reading_time field value to ${posts.length} posts.`);
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const post of posts) {
+            const readingTime = calculatePostReadingTime(post.html, post.feature_image);
+            await knex('posts').update('reading_time', readingTime).where('id', post.id);
+        }
+    },
+    async function down() {}
+);
